045533f3cdc3e39bec403eb788318d9775a52524
takiteke <ttt1997033@gmail.com>
2018-03-11 23:43:01 +0900

    second commit

---
+++ protos/cut_png.py
@@ -0,0 +1,26 @@
+import numpy as np
+from tqdm import tqdm
+
+from PIL import Image
+
+
+dataDir='../input/'
+tar_width = 512
+first_index = 0
+index = first_index
+for i in tqdm(range(0, 40)):
+    miki = Image.open(dataDir+"png/%02d_miki.png"%i)
+    ritsuko = Image.open(dataDir+"png/%02d_ritsuko.png"%i)
+
+    w = miki.size[0]
+    cut_num = int(w / tar_width)
+    for j in range(cut_num):
+        #cut
+        miki_cut = miki.crop((j*tar_width, 0, min((j+1)*tar_width, w), 513))
+        ritsuko_cut = ritsuko.crop((j*tar_width, 0, min((j+1)*tar_width, w), 513))
+
+        #cutしたデータをpngで保存
+        miki_cut.save(dataDir+"png_cut/%04d_miki.png"%index)
+        ritsuko_cut.save(dataDir+"png_cut/%04d_ritsuko.png"%index)
+
+        index += 1
--- protos/parameters.py
+++ protos/parameters.py
@@ -10,10 +10,10 @@ def set_params():
     hparams = AttrDict(
         # Audio:
         num_mels=80,
-        num_freq=1025,
+        num_freq=513,
         sample_rate=24000,
-        frame_length_ms=50, # 50/1000[s] * 28000[Hz] = 600個
-        frame_shift_ms=12.5,
+        frame_length_ms=40,
+        frame_shift_ms=10,
         preemphasis=0.97,
         min_level_db=-100,
         ref_level_db=20,
